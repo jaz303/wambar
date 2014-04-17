5623c019faf95af93344aa7848c0e87b05a0e6de
Jason Frame <jason@onehackoranother.com>
2014-04-17 09:10:21 +0100

    parsing node arguments now and some other stuff

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
 var wambar = require('./index').createSession();
 
-var graph = wambar('[ osc#osc1 -> eq, osc#osc2 -> eq, osc#osc3 -> eq ] -> osc');
+var graph = wambar('[ [ osc(a=-1.23,c=true) -> osc(type=sine) -> osc ], osc#osc1 -> eq, osc#osc2 -> eq, osc#osc3 -> eq ] -> [ osc ] -> osc');
+
+// wambar.define('channel-strip', 'osc -> filter -> lfo -> adsr');
+
+// wambar.add('ch1', 'channel-strip');
+// wambar.add('ch2', 'channel-strip');
+// wambar.add('ch3', 'channel-strip');
+
+
+
+
+// wambar.define('ch1');
 
 console.log(graph);
